a4644fb948cd5426ddd48a82b1c779020938d62e
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-03-30 11:15:03 -0400

    initial commit

---
+++ R/oncoKB-class.R
@@ -0,0 +1,56 @@
+.api_header <- function(x) x@api_header
+
+#' @name oncoKB-class
+#'
+#' @title A class for representing the oncoKB API protocol
+#'
+#' @description The `oncoKB` class is a representation of the `oncoKB`
+#'     API protocol that directly inherits from the `Service` class in the
+#'     `AnVIL` package. For more information, see the
+#'     \link[AnVIL:Service]{AnVIL} package.
+#'
+#' @details This class takes the static API as provided at
+#'     \url{https://www.oncoKB.org/api/api-docs} and creates an R object
+#'     with the help from underlying infrastructure (i.e.,
+#'     \link[rapiclient:rapiclient-package]{rapiclient} and
+#'     \link[AnVIL:Service]{AnVIL}) to give the user a unified representation
+#'     of the API specification provided by the oncoKB group. Users are not
+#'     expected to interact with this class other than to use it as input
+#'     to the functionality provided by the rest of the package.
+#'
+#' @importFrom methods new
+#'
+#' @seealso  \link{oncoKB}, \linkS4class{Service}
+#'
+#' @md
+#'
+#' @examples
+#'
+#' oncoKB()
+#'
+#' @export
+.oncoKB <- setClass(
+    "oncoKB",
+    contains = "Service",
+    slots = c(api_header = "character")
+)
+
+#' @describeIn oncoKB-class
+#'
+#' @importFrom AnVIL operations
+#' @importFrom methods callNextMethod
+#'
+#' @param x A \linkS4class{Service} instance or API representation as
+#'     given by the \link{oncoKB} function.
+#'
+#' @inheritParams AnVIL::operations
+#'
+#' @export
+setMethod(
+    "operations", "oncoKB",
+    function(x, ..., .deprecated = FALSE)
+{
+    callNextMethod(
+        x, .headers = .api_header(x), ..., .deprecated = .deprecated
+    )
+})
---
+++ R/oncoKB.R
@@ -0,0 +1,74 @@
+utils::globalVariables(c("clinicalAttributeId", "value", "sampleId"))
+
+.handle_token <- function(token) {
+    if (file.exists(token))
+        token <- readLines(token)
+    else if (grepl(.Platform$file.sep, token, fixed = TRUE))
+        stop("The token filepath is not valid")
+    token <- gsub("token: ", "", token)
+    c(Authorization = paste("Bearer", token))
+}
+
+#' @rdname oncoKB
+#'
+#' @aliases oncoKB
+#'
+#' @title The R interface to the oncoKB API Data Service
+#'
+#' @description This section of the documentation lists the functions that
+#'     allow users to access the oncoKB API. The main representation of the
+#'     API can be obtained from the `oncoKB` function. The supporting
+#'     functions listed here give access to specific parts of the API and
+#'     allow the user to explore the API with individual calls.
+#'
+#' @param hostname character(1) The internet location of the service
+#'     (default: 'www.cbioportal.org')
+#'
+#' @param protocol character(1) The internet protocol used to access the
+#'     hostname (default: 'https')
+#'
+#' @param api. character(1) The directory location of the API protocol within
+#'     the hostname (default: '/api/api-docs')
+#'
+#' @param token character(1) The Authorization Bearer token e.g.,
+#'     "63eba81c-2591-4e15-9d1c-fb6e8e51e35d" or a path to text file.
+#'
+#' @return
+#'
+#'     oncoKB: An API object of class 'oncoKB'
+#'
+#' @importFrom AnVIL Service
+#'
+#' @examples
+#' oncokb <- oncoKB()
+#'
+#' searchOps(api = cbio, keyword = "molecular")
+#'
+#' @export
+oncoKB <- function(
+    hostname = "www.oncokb.org",
+    protocol = "https",
+    api. = "/api/api-docs",
+    token = character()
+) {
+    if (length(token))
+        token <- .handle_token(token)
+
+    apiUrl <- paste0(protocol, "://", hostname, api.)
+    .oncoKB(
+        Service(
+            service = "oncoKB",
+            host = hostname,
+            config = httr::config(
+                ssl_verifypeer = 0L, ssl_verifyhost = 0L, http_version = 0L
+            ),
+            authenticate = FALSE,
+            api_reference_url = apiUrl,
+            api_reference_md5sum = "6abc321feb60da3251620743b527bab9",
+            api_reference_headers = token,
+            package = "oncoKB",
+            schemes = protocol
+        ),
+        api_header = token
+    )
+}
